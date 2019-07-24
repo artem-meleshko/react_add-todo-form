9ac4c7a840de6420b4abc6b1f7c2167b9f6db5cd
artem-meleshko <timonlondon@hotmail.com>
2019-07-24 17:29:52 +0300

    Updates

---
+++ precache-manifest.5a7e85ded7e4e04bff986798ffa6f248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fbde3f7db355e9d3003eee7792c110",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "ce28b6f51e366cb38887",
+    "url": "/react_add-todo-form/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "b29d1c0ac3cd4a21a8e9",
+    "url": "/react_add-todo-form/static/js/2.daa13e25.chunk.js"
+  },
+  {
+    "revision": "ce28b6f51e366cb38887",
+    "url": "/react_add-todo-form/static/js/main.4a5d831a.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
